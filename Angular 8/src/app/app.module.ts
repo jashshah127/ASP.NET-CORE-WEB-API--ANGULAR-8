1049dee0e10b5ce0c1d7e5e80c9f2018f2c2a30c
Jash Bhavesh Shah <46564241+jashshah127@users.noreply.github.com>
2020-01-29 17:30:45 +0530

    Here is the final code
    After perfectly implementing CRUD operations, and  a bit of styling, HERE IS THE FINAL CODE

---
+++ Angular 8/src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+ 
+import { ToastrModule } from 'ngx-toastr';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PaymentDetailComponent } from './payment-detail/payment-detail.component';
+import { PaymentsComponent } from './payment-detail/payments/payments.component';
+import { PaymentsListComponent } from './payment-detail/payments-list/payments-list.component';
+import { PaymentsService } from './shared/payments.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PaymentDetailComponent,
+    PaymentsComponent,
+    PaymentsListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot()
+  ],
+  providers: [PaymentsService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
